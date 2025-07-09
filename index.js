250b29fa5670f811fa1ff839c7de94785cb47bbb
inischal <nspnischal@gmail.com>
2025-07-09 14:59:40 +0545

    connected to database and created List Todo page

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = 8000;
 //initialize app
 const app = express();
 
+//database connection:
+const connectionUrl = "mongodb://127.0.0.1:27017/todoDb";
+mongoose.connect(connectionUrl).then(() => console.log("Database connection successful")).catch((error) => console.log(error.message));
+
+
 //view engine
 app.set("view engine", "ejs");
+app.get("/", (req, res, next) => {
+    try {
+       res.render("index")
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+})
 
 //listen server
 app.listen(PORT, () => {
